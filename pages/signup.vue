6998d6c19b040a6ac187a518b5e3ec2da5898827
hamza <hamza.badaoui@insa-lyon.fr>
2021-03-27 16:20:42 +0100

    init(signup): 🔑 creating signup page #7

---
+++ pages/signup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Sign up form</h1>
+    <div>
+      <ul>
+        <li v-for="field in form" :key="field.title">
+          <v-text-field :label="field.title" type="field.type"></v-text-field>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "signup",
+  auth: false,
+  data: function () {
+    return {
+      form: [
+        { title: "first name", type: "firstname" },
+        { title: "last name", type: "lastname" },
+        { title: "email", type: "email" },
+        { title: "phone", type: "phone" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
