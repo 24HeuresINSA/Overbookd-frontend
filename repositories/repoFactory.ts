36b544b78333b1326d08ec52c4c1bed7776a65c3
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-10-24 16:26:14 +0200

    feat(fa): 🔨 added fa repo

--- repositories/repoFactory.ts
+++ repositories/repoFactory.ts
@@ -1,9 +1,11 @@
 import userRepo from "./userRepo";
+import faRepo from "./faRepo";
 import transactionRepo from "~/repositories/transactionRepo";
 
 const repositories = {
   user: userRepo,
   transaction: transactionRepo,
+  FA: faRepo,
 };
 
 type repoKey = keyof typeof repositories;
@@ -12,4 +14,5 @@ export const RepoFactory = {
   get: (name: repoKey) => repositories[name],
   transactionRepo,
   userRepo,
+  faRepo,
 };
